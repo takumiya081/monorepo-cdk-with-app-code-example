ebb74beee72865994e75757406ab0381df7a03b5
takumiya081 <takumiya081@gmail.com>
2022-02-07 03:39:51 +0900

    add another app

--- services/pdf_download/infra/src/bin/cdk-example.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-
-import {CdkExampleStack} from './lib/cdk-example-stack';
-
-const app = new cdk.App();
-// eslint-disable-next-line no-new
-new CdkExampleStack(app, 'CdkExampleStack');
